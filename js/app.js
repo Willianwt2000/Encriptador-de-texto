a23f3d87158329977ae12f4367894e8faf1bac2b
willianwt2000 <willianmetelien@gmail.com>
2024-01-27 12:37:16 -0500

    agregando logica para desencriptar y copiar

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 const encriptar = document.getElementById('encriptar');
-const contenedorResultado = document.querySelector('.resultado');
+const desEncriptarResult = document.querySelector('.resultado');
 const btnEncriptar = document.getElementById('btnEncriptar');
+const btnDesencriptar = document.getElementById('btnDesencriptar');
+const btnCopiar = document.getElementById('btnCopiar');
 const imgExterminador = document.querySelector('.imgloader');
 
-function encriptarTexto() {
+const encriptarTexto = () => {
   try {
     let texto = encriptar.value.trim().toLowerCase();
     let contieneNumeros = /[^A-Za-z\s]/.test(texto);
 
-    if (contieneNumeros) throw new Error('Texto de encriptación inválido. ¡Ingresa algo digno de ser asegurado, humano!');
-    if (texto.trim() === '') throw new Error('¡No hay texto para encriptar! Ingresa algo digno de ser asegurado.');
+    if (contieneNumeros) throw new Error('Texto de encriptación inválido.\n¡Ingresa algo digno de ser asegurado, humano!');
+    if (texto.trim() === '') throw new Error('¡No hay texto para encriptar!\nIngresa algo digno de ser asegurado.');
 
-    const textoEncriptado = texto.replace(/[aeiou]/g, (frase) => {
+    const textoEncriptado = texto.replace(/[aeiou]/gi, (frase) => {
       switch (frase) {
         case 'a':
           return 'ai';
@@ -27,7 +29,7 @@ function encriptarTexto() {
     });
 
     // Establecer el texto encriptado como contenido del contenedor
-    contenedorResultado.textContent = textoEncriptado;
+    desEncriptarResult.textContent = textoEncriptado;
 
     // Ocultar el contenido del contenedor
     imgExterminador.style.display = 'none';
@@ -37,4 +39,54 @@ function encriptarTexto() {
   }
 }
 
+//Funcion desencriptar
+const desEncriptar = () =>  {
+  try {
+    let textoEncriptado = encriptar.value
+    let contieneNumeros = /[^A-Za-z\s]/.test(textoEncriptado);
+
+    if (contieneNumeros) throw new Error('Texto de desencriptación inválido.\n¡Ingresa algo digno de ser asegurado, humano!');
+    if (textoEncriptado === '') throw new Error('¡No hay texto para desencriptar!\nIngresa algo digno de ser asegurado.');
+
+    const desEncriptado = textoEncriptado.replace(/ai|enter|imes|ober|ufat/g, (frase) => {
+      switch (frase) {
+        case 'ai':
+          return 'a';
+        case 'enter':
+          return 'e';
+        case 'ober':
+          return 'o';
+        case 'imes':
+          return 'i';
+        case 'ufat':
+          return 'u';
+      }
+    });
+
+    // Establecer el texto encriptado como contenido del contenedor
+    desEncriptarResult.textContent = desEncriptado;
+
+
+    imgExterminador.style.display = 'none';
+
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+
+//llamadas
 btnEncriptar.addEventListener('click', encriptarTexto);
+btnDesencriptar.addEventListener('click', desEncriptar);
+
+//funcion para copiar
+btnCopiar.addEventListener('click', () => {
+  const range = document.createRange();
+  range.selectNode(desEncriptarResult);
+  const seleccion = window.getSelection();
+  seleccion.removeAllRanges();
+  seleccion.addRange(range);
+  document.execCommand('copy');
+  seleccion.removeAllRanges();
+  alert('Texto replicado y asegurado en el portapapeles. No escaparás: ');
+});
